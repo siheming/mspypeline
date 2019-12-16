a9f0dc5002c2fb66f0c6c2bd9b83ade5c32ea6ce
sheming <s.heming@dfkz.de>
2019-12-16 12:54:00 +0100

    module call starts the pipeline

--- mqpipeline/__init__.py
+++ mqpipeline/__init__.py
@@ -10,3 +10,18 @@ __all__ = [
     "MQParser",
     "MQGUI"
 ]
+
+if __name__ == "__main__":
+    import logging
+    mqparser = MQParser()
+
+    # determine logging level
+    try:
+        loglevel = getattr(logging, mqparser.args.loglevel.upper())
+    except AttributeError:
+        try:
+            loglevel = int(mqparser.args.loglevel)
+        except ValueError:
+            loglevel = logging.DEBUG
+
+    gui = MQGUI(mqparser.args, loglevel=loglevel)
