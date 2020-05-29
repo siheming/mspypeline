65a220aa3bd7b9cae22eb93a5a8e642de616416e
sheming <s.heming@dfkz.de>
2020-05-29 18:22:46 +0200

    fixed bug with missing kwargs in plot_r_volcano

--- mspypeline/core/MSPPlots.py
+++ mspypeline/core/MSPPlots.py
@@ -699,7 +699,7 @@ class MSPPlots:
             for g1, g2 in combinations(level_keys, 2):
                 data = self.get_r_volcano_data(g1, g2, df_to_use, level)
                 if data:
-                    plot_kwargs = dict(g1=g1, g2=g2, save_path=self.file_dir_volcano,
+                    plot_kwargs = dict(g1=g1, g2=g2, save_path=self.file_dir_volcano, df_to_use=df_to_use, level=level,
                                        intensity_label=self.intensity_label_names[df_to_use])
                     plot_kwargs.update(**kwargs)
                     matplotlib_plots.save_volcano_results(**data, **plot_kwargs)
