86ac4ea2690be5ba078c6ea04cb5e7d2d3ec82a5
Simon Heming <simon.heming@gmx.de>
2021-03-02 13:40:39 +0100

    implemented method to download the dataset from github

--- mspypeline/__init__.py
+++ mspypeline/__init__.py
@@ -31,7 +31,28 @@ __all__.extend(modules.__all__)
 __all__.extend(file_reader.__all__)
 
 
-def load_example_dataset(path_dataset: str = None, configs: dict = None):
+def load_example_dataset(
+        path_dataset: str = None, configs: dict = None, download: bool = False, remove_on_fail: bool = True
+) -> MSPInitializer:
+    """
+    Loads the example dataset into a MSPInitializer. If download is true the data is downloaded from github.
+
+    Parameters
+    ----------
+    path_dataset
+        path to where the dataset should be read from and / or downloaded to
+    configs
+        configs that will be passed to the initializer configs
+    download
+        should the data be downloaded
+    remove_on_fail
+        should the passed path_dataset directory be removed if the download fails
+
+    Returns
+    -------
+    An initializer with all data loaded into memory.
+
+    """
     import pandas as pd
 
     if configs is None:
@@ -39,20 +60,35 @@ def load_example_dataset(path_dataset: str = None, configs: dict = None):
     if path_dataset is None:
         path_dataset = os.path.join(os.path.split(path_package)[0], "mspypeline_public_dataset")
     path_dataset_txt = os.path.join(path_dataset, "txt")
+
+    file_list = ["contaminants", "evidence", "msmsScans", "msScans", "parameters", "peptides", "proteinGroups",
+                 "summary"]
+    if download:
+        assert int(pd.__version__.split(".")[0]) >= 1, "Pandas Version must be at least 1.0 to download"
+        try:
+            url_base = "https://raw.githubusercontent.com/siheming/mspypeline_public_dataset/main/txt/"
+            os.makedirs(path_dataset_txt, exist_ok=True)
+            for file in file_list:
+                pd.read_pickle(url_base + file + ".gzip").to_pickle(os.path.join(path_dataset_txt, file + ".gzip"))
+            pd.read_csv(url_base + "proteinGroups.txt", sep="\t"
+                        ).to_csv(os.path.join(path_dataset_txt, "proteinGroups.txt"), sep="\t")
+        except Exception as e:
+            if remove_on_fail:
+                import shutil
+                shutil.rmtree(path_dataset_txt)
+            raise e
+
     init = MSPInitializer(path_dataset)
     init.init_config()
     init.file_path_yaml = "default"
     init.configs.update(configs)
     init.read_data()
 
-    init.reader_data["mqreader"]["contaminants"] = pd.read_pickle(os.path.join(path_dataset_txt, "contaminants.gzip"))
-    init.reader_data["mqreader"]["evidence"] = pd.read_pickle(os.path.join(path_dataset_txt, "evidence.gzip"))
-    init.reader_data["mqreader"]["msmsScans"] = pd.read_pickle(os.path.join(path_dataset_txt, "msmsScans.gzip"))
-    init.reader_data["mqreader"]["msScans"] = pd.read_pickle(os.path.join(path_dataset_txt, "msScans.gzip"))
-    init.reader_data["mqreader"]["parameters"] = pd.read_pickle(os.path.join(path_dataset_txt, "parameters.gzip"))
-    init.reader_data["mqreader"]["peptides"] = pd.read_pickle(os.path.join(path_dataset_txt, "peptides.gzip"))
-    init.reader_data["mqreader"]["proteinGroups"] = pd.read_pickle(os.path.join(path_dataset_txt, "proteinGroups.gzip"))
-    init.reader_data["mqreader"]["summary"] = pd.read_pickle(os.path.join(path_dataset_txt, "summary.gzip"))
+    try:
+        for file in file_list:
+            init.reader_data["mqreader"][file] = pd.read_pickle(os.path.join(path_dataset_txt, file + ".gzip"))
+    except FileNotFoundError:
+        raise FileNotFoundError("The files are not under the specified directory. Try specifying download=True")
     return init
 
 
